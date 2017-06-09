a908f94d9bdef213d4d4e69a8df2c713d0c5ff58
Ignacio Nieto <contact@digitalleaves.com>
2017-06-09 18:33:06 +0200

    Initial commit

--- BrokenQuiz/ViewController.swift
+++ BrokenQuiz/ViewController.swift
@@ -9,17 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var quizLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var checkResponseButton: UIButton!
 
+    // data
+    var number1: Int!
+    var number2: Int!
+    var userHasAnswered = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Update UI
+        containerView.layer.cornerRadius = 6.0
+        checkResponseButton.layer.cornerRadius = 6.0
+        
+        // Start game
+        generateQuiz()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func generateQuiz() {
+        // generate two random numbers
+        number1 = Int(arc4random_uniform(10)) + 1
+        number2 = Int(arc4random_uniform(10)) + 1
 
+        // update UI
+        answerTextField.text = ""
+        quizLabel.text = "How much is \(number1!) + \(number2!)?"
+        checkResponseButton.setTitle("Check response", for: .normal)
+    }
+    
+    func checkQuiz(_ response: String) {
+        // first, parse the response string as an int
+        
+        // BUG 2. Parsing an integer will return nil if the string is not a number.
+        let responseAsNumber = Int(response)!
+        /* SOLUTION 2. Check that the answer is actually a number
+        guard let responseAsNumber = Int(response) else {
+            quizLabel.text = "Sorry, that was not even a number! 😱"
+            checkResponseButton.setTitle("Play again!", for: .normal)
+            return
+        }
+         */
+        
+        // then, check it.
+        // BUG 2. We did a mistake in the operation, the result is not calculated correctly
+        if responseAsNumber == number1 - number2 { // win!
+        // SOLUTION 2. Fix the calculation: 
+        // if responseAsNumber == number1 + number2 { // win!
+            quizLabel.text = "Congratulations! You win! 🎉🎉🎉"
+        } else { // loose!
+            quizLabel.text = "Sorry, the right answer was \(number1 + number2)... 😓"
+        }
+        
+        // update UI
+        checkResponseButton.setTitle("Play again!", for: .normal)
+    }
+    
+    @IBAction func checkResponse(_ sender: Any) {
+        if answerTextField.text == nil || answerTextField.text!.characters.count < 1 { return }
+        
+        if userHasAnswered { // reset quiz
+            generateQuiz()
+        } else { // check response
+            checkQuiz(answerTextField.text!)
+        }
+        
+        userHasAnswered = !userHasAnswered
+        answerTextField.isHidden = userHasAnswered
+    }
 
 }
 
